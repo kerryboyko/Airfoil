2b8a063472e1da4c67a6b4e733e5ea16d3efdcad
Brian Boyko <briboyko@publicisgroupe.net>
2021-04-26 02:06:49 +0100

    Failing tests: just need to save before bed

--- airfoil-demo/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from 'preact'
-import Routes from './Routes'
-import './index.css'
-
-render(<Routes />, document.getElementById('app')!)
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ["<rootDir>/airfoil"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
